9cd5bb98126bdaf041aba78751c1a01cb95c2cff
Edgar Zavala <ezavile@gmail.com>
2017-01-28 01:35:06 -0600

    Adding BookDetail Container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import books from './reducer_books';
+import activeBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   books,
+  activeBook,
 });
 
 export default rootReducer;
